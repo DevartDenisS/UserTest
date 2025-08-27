dce02cbf10f7b0aaed2fc1025b97a23698789738
DenisS <deniss@devart.com>
2025-08-27 17:31:18 +0300

    2

--- Tables/dbo.dm_product.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[dm_product] (
-  [product_id] [int] IDENTITY,
-  [product_name] [nvarchar](100) NOT NULL,
-  [price] [decimal](10, 2) NOT NULL,
-  PRIMARY KEY CLUSTERED ([product_id])
-)
-ON [PRIMARY]
-GO
