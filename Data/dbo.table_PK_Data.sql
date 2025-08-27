7afdc95b21eead95624364543c2e73ab6f5160c8
DevartDenisS <132042766+DevartDenisS@users.noreply.github.com>
2025-08-27 17:15:27 +0300

    Delete Data directory

--- Data/dbo.table_PK_Data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO [dbo].[table_PK] ([uk], [c1]) VALUES (1001, 100)
-INSERT INTO [dbo].[table_PK] ([uk], [c1]) VALUES (1002, 200)
-INSERT INTO [dbo].[table_PK] ([uk], [c1]) VALUES (3001, 10)
-INSERT INTO [dbo].[table_PK] ([uk], [c1]) VALUES (3002, 20)
