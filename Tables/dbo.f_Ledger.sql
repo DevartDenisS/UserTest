ae8a447a5416031e7a161b663b721027dcc0c174
DenisS <deniss@devart.com>
2025-09-02 16:08:03 +0300

    1

---
+++ Tables/dbo.f_Ledger.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[f_Ledger] (
+  [prtId] [int] NOT NULL,
+  [aKey] [int] NOT NULL,
+  [cKey] [int] NOT NULL,
+  [SomeData] [nvarchar](100) NULL
+)
+ON [prtId_Schema] ([prtId])
+GO
+
+CREATE INDEX [Idx_aKey]
+  ON [dbo].[f_Ledger] ([prtId], [aKey])
+  WITH (FILLFACTOR = 100)
+  ON [prtId_Schema] ([prtId])
+GO
+
+CREATE INDEX [Idx_cKey]
+  ON [dbo].[f_Ledger] ([prtId], [cKey])
+  WITH (FILLFACTOR = 100)
+  ON [prtId_Schema] ([prtId])
+GO
