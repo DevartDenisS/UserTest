2faee795bae101e015c9c780c96147ed22c4504e
DevartDenisS <132042766+DevartDenisS@users.noreply.github.com>
2025-09-02 10:43:40 +0300

    Delete Tables directory

--- Tables/dbo.table_PK.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[table_PK] (
-  [uk] [int] NOT NULL,
-  [c1] [int] NULL,
-  [c2] [nvarchar](50) NOT NULL DEFAULT ('Test'),
-  [c3] [nvarchar](50) NOT NULL DEFAULT ('Test'),
-  PRIMARY KEY CLUSTERED ([uk])
-)
-ON [PRIMARY]
-GO
