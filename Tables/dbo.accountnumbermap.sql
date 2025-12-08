c51fefc35c30f123a15151d28ca9f6eea436ff0b
DenisS <deniss@devart.com>
2025-12-08 16:06:15 +0200

    3

---
+++ Tables/dbo.accountnumbermap.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[accountnumbermap] (
+  [id] [int] IDENTITY,
+  [account_number] [varchar](30) MASKED WITH (FUNCTION = 'default()') NOT NULL,
+  [account_id] [int] NOT NULL,
+  [accountnumbertype_id] [int] NULL,
+  [update_time] [datetime] NOT NULL DEFAULT (suser_sname()),
+  [update_by] [varchar](35) NULL DEFAULT (suser_sname()),
+  CONSTRAINT [pk__accountn__3213e83f45b4effa] PRIMARY KEY CLUSTERED ([id])
+)
+GO
--- Tables/dbo.t_masked_column.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[t_masked_column] (
-  [id] [int] IDENTITY,
-  [account_number] [varchar](30) MASKED WITH (FUNCTION = 'default()') NULL,
-  [account_id] [int] NOT NULL,
-  [update_time] [datetime] NOT NULL
-)
-GO
