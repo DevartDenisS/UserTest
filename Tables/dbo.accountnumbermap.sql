53bdcc83f5adac194a46b2632389f1b3df000909
DenisS <deniss@devart.com>
2025-12-08 16:16:16 +0200

    mask

--- Tables/dbo.accountnumbermap.sql
+++ Tables/dbo.accountnumbermap.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[accountnumbermap] (
   [id] [int] IDENTITY,
-  [account_number] [varchar](30) MASKED WITH (FUNCTION = 'default()') NOT NULL,
+  [account_number] [varchar](30) NOT NULL,
   [account_id] [int] NOT NULL,
   [accountnumbertype_id] [int] NULL,
   [update_time] [datetime] NOT NULL DEFAULT (suser_sname()),
