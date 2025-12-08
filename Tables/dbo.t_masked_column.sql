bc088ad1c472f11c8dee2b66cac1447280d5993a
DenisS <deniss@devart.com>
2025-12-08 15:55:11 +0200

    3

--- Tables/dbo.t_masked_column.sql
+++ Tables/dbo.t_masked_column.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[t_masked_column] (
   [id] [int] IDENTITY,
-  [account_number] [varchar](30) MASKED WITH (FUNCTION = 'default()') NULL,
+  [account_number] [varchar](30) NULL,
   [account_id] [int] NOT NULL,
   [update_time] [datetime] NOT NULL
 )
