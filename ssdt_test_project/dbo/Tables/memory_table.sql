0fd4792f5faf264f31f547c4528490de4bd59a50
DenisS <deniss@devart.com>
2026-05-25 13:25:00 +0300

    2

--- ssdt_test_project/dbo/Tables/memory_table.sql
+++ ssdt_test_project/dbo/Tables/memory_table.sql
@@ -1,7 +1,2 @@
-﻿CREATE TABLE [dbo].[memory_table] (
-    [id]         INT            NOT NULL,
-    [value_text] NVARCHAR (100) NULL,
-    PRIMARY KEY NONCLUSTERED ([id] ASC)
-)
-WITH (MEMORY_OPTIMIZED = ON);
-
+﻿
+;
